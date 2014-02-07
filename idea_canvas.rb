fb06a90e296c3e34969f64df611d491816bd8fcd
Carlos Vilhena <carlosvilhena@gmail.com>
2014-02-07 13:37:35 +0000

    compile all the things

--- idea_canvas.rb
+++ idea_canvas.rb
@@ -1,10 +1,12 @@
+require 'bundler'
 Bundler.require
 
 require 'sinatra/asset_pipeline'
 
 class IdeaCanvas < Sinatra::Base
 
-  set :assets_prefix, %w(assets)
+  set :assets_precompile, %w(*.css *.png *.jpg *.svg)
+  set :assets_prefix, %w(assets assets/stylesheets/ assets/images/)
 
   register Sinatra::AssetPipeline
 
